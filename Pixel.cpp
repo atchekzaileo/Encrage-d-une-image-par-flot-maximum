c43014f0e5a3bbe5c48b19f96c35b159cd016aa7
atchekzaileo <atchekzaileo69@gmail.com>
2025-12-25 23:56:45 +0100

    Update Pixel.cpp

--- Pixel.cpp
+++ Pixel.cpp
@@ -1,11 +1,8 @@
 #include "Pixel.h"
 
-// Constructeur par défaut.
-// On met intensité, capacités et flots à 0.
-Pixel::Pixel() {
-    intensite = 0;
+Pixel::Pixel() : intensite(0) {
     for (int i = 0; i < 6; ++i) {
-        cap[i]  = 0;
+        cap[i] = 0;
         flot[i] = 0;
     }
 }
